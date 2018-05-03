56cb0a2c9eb96f9cd3b5ba3eedf71397a174c0f4
Muntaser Qutub <muntaser.qutub@relativity.com>
2018-05-02 19:15:35 -0500

    Changes to the way the nav bar icons are rendered

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Platform } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import { TabNavigator, TabBarBottom } from 'react-navigation';
-
 import Colors from '../constants/Colors';
-
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
+import QRScreen from '../screens/QRScreen';
 import ReceiptsScreen from '../screens/ReceiptsScreen';
 import SettingsScreen from '../screens/SettingsScreen';
+
 export default TabNavigator(
   {
     Home: {
       screen: HomeScreen,
+    },
+	 QRCode: {
+      screen: QRScreen,
     },
     Receipts: {
       screen: ReceiptsScreen,
@@ -28,20 +32,19 @@ export default TabNavigator(
         let iconName;
         switch (routeName) {
           case 'Home':
-            iconName =
-              Platform.OS === 'ios'
-                ? `ios-home${focused ? '' : '-outline'}`
-                : 'md-home';
+            iconName = "home-account"
             break;
           case 'Receipts':
-            iconName = Platform.OS === 'ios' ? `ios-paper${focused ? '' : '-outline'}` : 'ios-paper';
+            iconName = "table-column"
+            break;
+          case 'QRCode':
+            iconName = "qrcode-scan"
             break;
           case 'Settings':
-            iconName =
-              Platform.OS === 'ios' ? `ios-construct${focused ? '' : '-outline'}` : 'ios-construct';
+            iconName = "settings"
         }
         return (
-          <Ionicons
+          <Icon
             name={iconName}
             size={28}
             style={{ marginBottom: -3 }}
